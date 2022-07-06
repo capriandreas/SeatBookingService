e236c737b0248e43413051e73058e23e7f7c2a19
Andreas Capri <capriandreas@gmail.com>
2022-07-06 17:32:16 +0700

    Commit perbaikan bugs tipe non reguler di Get List Cancel Seat dan Get History Header

--- SeatBookingService/Models/DAO/TransactionDao.cs
+++ SeatBookingService/Models/DAO/TransactionDao.cs
@@ -160,9 +160,11 @@ namespace SeatBookingService.Models.DAO
 								) 
 								when b.trip_type_id = 2 then 
                                 (
-									select CONCAT_WS (' - ', a.origin, a.destination) as `Route`
-									from tr_trip_schedule a
-									where a.id = b.route_id
+									select GROUP_CONCAT(a.city order by a.route_order separator ' - ') as `Route`
+									from tr_trip_schedule_routes a
+									left join tr_trip_schedule b on b.id = a.trip_schedule_id
+                                    where a.id = b.route_id
+									group by b.id
 								) end as route,
                                 case 
 								when b.trip_type_id = 1 then 
@@ -361,9 +363,11 @@ namespace SeatBookingService.Models.DAO
 								) 
 								when f.trip_type_id = 2 then 
                                 (
-									select CONCAT_WS (' - ', a.origin, a.destination) as `Route`
-									from tr_trip_schedule a
-									where a.id = f.route_id
+									select GROUP_CONCAT(a.city order by a.route_order separator ' - ') as `Route`
+									from tr_trip_schedule_routes a
+									left join tr_trip_schedule b on b.id = a.trip_schedule_id
+                                    where a.id = f.route_id
+									group by b.id
 								) 
 							end as route,
                             case 
